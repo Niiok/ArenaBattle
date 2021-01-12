c47258dcf27ec432efd2edc432a401e1537c9b20
Niiok <play.niiok@gmail.com>
2021-01-12 22:35:50 +0900

    Character View Setting

--- Source/ArenaBattle/ABCharacter.cpp
+++ Source/ArenaBattle/ABCharacter.cpp
@@ -38,7 +38,12 @@ AABCharacter::AABCharacter()
 		ABLOG(Error, TEXT("Animation blueprint haven't adjusted."));
 	}
 
+	SetControlMode(EControlMode::Basic);
+
+	ArmLengthSpeed = 3.0f;
+	ArmRotationSpeed = 10.0f;
 }
+
 
 // Called when the game starts or when spawned
 void AABCharacter::BeginPlay()
@@ -47,21 +52,92 @@ void AABCharacter::BeginPlay()
 	
 }
 
+
+void AABCharacter::SetControlMode(EControlMode NewControlMode)
+{
+	CurrentControlMode = NewControlMode;
+
+	switch(CurrentControlMode)
+	{
+	case EControlMode::Basic: //ThirdPerson Basic
+		//SpringArm->TargetArmLength = 450.0f;
+		//SpringArm->SetRelativeRotation(FRotator::ZeroRotator);
+		ArmLengthTo = 450.0f;
+		SpringArm->bUsePawnControlRotation = true;
+		SpringArm->bInheritPitch = true;
+		SpringArm->bInheritRoll = true;
+		SpringArm->bInheritYaw = true;
+		SpringArm->bDoCollisionTest = true;
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = true;
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+		break;
+
+	case EControlMode::Topview: //ThirdPerson TopView
+		//SpringArm->TargetArmLength = 800.0f;
+		//SpringArm->SetRelativeRotation(FRotator(-45.0f, 0.0f, 0.0f));
+		ArmLengthTo = 800.0f;
+		ArmRotationTo = FRotator(-45.0f, 0.0f, 0.0f);
+		SpringArm->bUsePawnControlRotation = false;
+		SpringArm->bInheritPitch = false;
+		SpringArm->bInheritRoll = false;
+		SpringArm->bInheritYaw = false;
+		SpringArm->bDoCollisionTest = false;
+		bUseControllerRotationYaw = false;
+		GetCharacterMovement()->bOrientRotationToMovement = false;
+		GetCharacterMovement()->bUseControllerDesiredRotation = true;
+		GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+		break;
+
+	default:
+		break;
+	}
+}
+
+
 // Called every frame
 void AABCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	//Velocity = sqrt(pow(GetVelocity().X,2)+pow(GetVelocity().Y,2)+pow(GetVelocity().Z,2));
+
+	SpringArm->TargetArmLength = FMath::FInterpTo(SpringArm->TargetArmLength, ArmLengthTo, DeltaTime, ArmLengthSpeed);
+
+	switch (CurrentControlMode)
+	{
+	case EControlMode::Basic:
+
+		break;
+
+	case EControlMode::Topview:
+		SpringArm->RelativeRotation = FMath::RInterpTo(SpringArm->RelativeRotation, ArmRotationTo, DeltaTime, ArmRotationSpeed);
+
+		if (DirectionToMove.SizeSquared() > 0.0f)
+		{
+			GetController()->SetControlRotation(FRotationMatrix::MakeFromX(DirectionToMove).Rotator());
+			AddMovementInput(DirectionToMove);
+		}
+		break;
+
+	default:
+		break;
+	}
 
 }
+
 
 // Called to bind functionality to input
 void AABCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction(TEXT("ViewChange"), EInputEvent::IE_Pressed, this, &AABCharacter::ViewChange);
+
 	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AABCharacter::UpDown);
 	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AABCharacter::LeftRight);
 	PlayerInputComponent->BindAxis(TEXT("LookUp"), this, &AABCharacter::LookUp);
+	PlayerInputComponent->BindAxis(TEXT("LookUpRate"), this, &AABCharacter::LookUp);
 	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &AABCharacter::Turn);
 	PlayerInputComponent->BindAxis(TEXT("TurnRate"),this,&AABCharacter::Turn);
 	//PlayerInputComponent->BindAxis(TEXT("Kill"),this,&AABCharacter::KillRag);
@@ -70,22 +146,63 @@ void AABCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 
 void AABCharacter::UpDown(float NewAxisValue)
 {
-	AddMovementInput(GetActorForwardVector(), NewAxisValue);
+	switch (CurrentControlMode)
+	{
+	case EControlMode::Basic:
+		//Ud = NewAxisValue;
+		//if (((Ud * Ud) + (Lr * Lr)) > 1)
+		//	NewAxisValue = NewAxisValue/sqrt((Ud * Ud) + (Lr * Lr));
+		AddMovementInput(FRotationMatrix(GetControlRotation()).GetUnitAxis(EAxis::X), NewAxisValue);
+		break;
+
+	case EControlMode::Topview:
+		DirectionToMove.X = NewAxisValue;
+		break;
+	}
 }
 
 void AABCharacter::LeftRight(float NewAxisValue)
 {
-	AddMovementInput(GetActorRightVector(), NewAxisValue);
+	switch (CurrentControlMode)
+	{
+	case EControlMode::Basic:
+		//Lr = NewAxisValue;
+		//if (((Ud * Ud) + (Lr * Lr)) > 1)
+		//	NewAxisValue = NewAxisValue / sqrt((Ud * Ud) + (Lr * Lr));
+		AddMovementInput(FRotationMatrix(GetControlRotation()).GetUnitAxis(EAxis::Y), NewAxisValue);
+		break;
+
+	case EControlMode::Topview:
+		DirectionToMove.Y = NewAxisValue;
+		break;
+	}
 }
 
 void AABCharacter::LookUp(float NewAxisValue)
 {
-	AddControllerPitchInput(NewAxisValue);
+	switch (CurrentControlMode)
+	{
+	case EControlMode::Basic:
+		AddControllerPitchInput(NewAxisValue);
+		break;
+
+	case EControlMode::Topview:
+		break;
+	}
 }
 
 void AABCharacter::Turn(float NewAxisValue)
 {
-	AddControllerYawInput(NewAxisValue);
+	switch (CurrentControlMode)
+	{
+	case EControlMode::Basic:
+		AddControllerYawInput(NewAxisValue);
+		break;
+
+	case EControlMode::Topview:
+		
+		break;
+	}
 }
 
 /*
@@ -97,3 +214,19 @@ void AABCharacter::KillRag(float NewActionValue)
 	ABLOG_S(Error);
 }
 */
+
+
+void AABCharacter::ViewChange()
+{
+	if (CurrentControlMode == EControlMode::Basic)
+	{
+		GetController()->SetControlRotation(GetActorRotation());
+		SetControlMode(EControlMode::Topview);
+	}
+	else
+	{
+		GetController()->SetControlRotation(SpringArm->RelativeRotation);
+		SetControlMode(CurrentControlMode = EControlMode::Basic);
+	}
+	
+}
--- Source/ArenaBattle/ABCharacter.h
+++ Source/ArenaBattle/ABCharacter.h
@@ -15,9 +15,29 @@ public:
 	// Sets default values for this character's properties
 	AABCharacter();
 
+
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	enum class EControlMode
+	{
+		Basic,
+		Topview,
+		Default
+	};
+
+	void SetControlMode(EControlMode NewControlMode);
+	EControlMode CurrentControlMode = EControlMode::Default;
+	FVector DirectionToMove = FVector::ZeroVector;
+
+	float ArmLengthTo = 0.0f;
+	FRotator ArmRotationTo = FRotator::ZeroRotator;
+	float ArmLengthSpeed = 0.0f;
+	float ArmRotationSpeed = 0.0f;
+
+
 
 public:	
 	// Called every frame
@@ -32,10 +52,20 @@ public:
 	UPROPERTY(VisibleAnywhere, Category = Camera)
 		UCameraComponent* Camera;
 	
+
+
 private:
 	void UpDown(float NewAxisValue);
 	void LeftRight(float NewAxisValue);
 	void LookUp(float NewAxisValue);
 	void Turn(float NewAxisValue);
 	//void KillRag(float NewAxisValue);
+
+	void ViewChange();
+
+	float Ud = 0.0f, Lr = 0.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	float Velocity;
+
 };
